26ced143bb8fd3b380f21424d565dc8f25d6c02b
tahsinnur <tahsinnurtajin@gmail.com>
2021-10-12 22:09:04 +0600

    first commit

---
+++ src/Pages/Home/Service/Service.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Service.css';
+
+const Service = ({service}) => {
+    const {name, img, price, description} = service;
+    return (
+        <div className="service">
+            <img src={img} alt="" />
+            <h3>{name}</h3>
+            <h5>Price: {price}</h5>
+            <p className="px-3">{description}</p>
+        </div>
+    );
+};
+
+export default Service;
